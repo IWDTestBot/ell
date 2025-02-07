51b277a3c1883391f3bd6a1717242a64fcbf9a3b
Marcel Holtmann <marcel@holtmann.org>
2025-02-07 21:44:46 +0100

    test: Add support for TAP, the Test Anything Protocol

--- ell/test.c
+++ ell/test.c
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
 #include "log.h"
 #include "test.h"
@@ -30,10 +31,16 @@ struct test {
 	l_test_func_t function;
 	const void *test_data;
 	struct test *next;
+	unsigned int num;
 };
 
 static struct test *test_head;
 static struct test *test_tail;
+static unsigned int test_count;
+
+static bool cmd_list;
+static bool tap_enable;
+static bool debug_enable;
 
 /**
  * l_test_init:
@@ -44,10 +51,43 @@ static struct test *test_tail;
  **/
 LIB_EXPORT void l_test_init(int *argc, char ***argv)
 {
+	static const struct option options[] = {
+		{ "list",	no_argument,	NULL, 'l' },
+		{ "text",	no_argument,	NULL, 't' },
+		{ "debug",	no_argument,	NULL, 'd' },
+		{ }
+	};
+
 	test_head = NULL;
 	test_tail = NULL;
+	test_count = 0;
 
 	l_log_set_stderr();
+
+	cmd_list = false;
+	tap_enable = true;
+	debug_enable = false;
+
+	for (;;) {
+		int opt;
+
+		opt = getopt_long(*argc, *argv, "ltd", options, NULL);
+		if (opt < 0)
+			break;
+
+		switch (opt) {
+		case 'l':
+			cmd_list = true;
+			tap_enable = false;
+			break;
+		case 't':
+			tap_enable = false;
+			break;
+		case 'd':
+			debug_enable = true;
+			break;
+		}
+	}
 }
 
 /**
@@ -61,20 +101,35 @@ LIB_EXPORT int l_test_run(void)
 {
 	struct test *test = test_head;
 
+	if (tap_enable) {
+		printf("TAP version 12\n");
+		printf("1..%u\n", test_count);
+	}
+
+	if (debug_enable)
+		l_debug_enable("*");
+
 	while (test) {
 		struct test *tmp = test;
 
-		printf("TEST: %s\n", test->name);
+		if (!tap_enable)
+			printf("TEST: %s\n", test->name);
 
-		test->function(test->test_data);
+		if (!cmd_list) {
+			test->function(test->test_data);
+			if (tap_enable)
+				printf("ok %u - %s\n", test->num, test->name);
+		}
 
 		test = test->next;
+		test_head = test;
 
 		free(tmp);
 	}
 
 	test_head = NULL;
 	test_tail = NULL;
+	test_count = 0;
 
 	return 0;
 }
@@ -112,4 +167,6 @@ LIB_EXPORT void l_test_add(const char *name, l_test_func_t function,
 
 	if (!test_head)
 		test_head = test;
+
+	test->num = ++test_count;
 }
