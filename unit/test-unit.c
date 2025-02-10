84267fc11cb22b120433eb3f08744d94d387cae1
Marcel Holtmann <marcel@holtmann.org>
2025-02-10 21:52:51 +0100

    unit: Test basic D-Bus bus setup requirement with test-unit

--- unit/test-unit.c
+++ unit/test-unit.c
@@ -33,6 +33,40 @@ static void test_add_func_fail(const void *data)
 	assert(0);
 }
 
+static void ready_callback(void *user_data)
+{
+	l_info("D-Bus ready");
+	l_main_quit();
+}
+
+static void disconnect_callback(void *user_data)
+{
+	l_info("D-Bus disconnect");
+	l_main_quit();
+}
+
+static void test_dbus_system_bus(const void *data)
+{
+	struct l_dbus *dbus;
+
+	dbus = l_dbus_new_default(L_DBUS_SYSTEM_BUS);
+	assert(dbus);
+
+	l_dbus_set_ready_handler(dbus, ready_callback, NULL, NULL);
+	l_dbus_set_disconnect_handler(dbus, disconnect_callback, NULL, NULL);
+}
+
+static void test_dbus_session_bus(const void *data)
+{
+	struct l_dbus *dbus;
+
+	dbus = l_dbus_new_default(L_DBUS_SESSION_BUS);
+	assert(dbus);
+
+	l_dbus_set_ready_handler(dbus, ready_callback, NULL, NULL);
+	l_dbus_set_disconnect_handler(dbus, disconnect_callback, NULL, NULL);
+}
+
 int main(int argc, char *argv[])
 {
 	l_test_init(&argc, &argv);
@@ -42,6 +76,11 @@ int main(int argc, char *argv[])
 	l_test_add_data_func("add-data-func", NULL, test_add_data_func, 0);
 	l_test_add_func("add-func-fail", test_add_func_fail,
 						L_TEST_FLAG_FAILURE_EXPECTED);
+
+	l_test_add_func("dbus-system-bus", test_dbus_system_bus,
+					L_TEST_FLAG_REQUIRE_DBUS_SYSTEM_BUS);
+	l_test_add_func("dbus-session-bus", test_dbus_session_bus,
+					L_TEST_FLAG_REQUIRE_DBUS_SESSION_BUS);
 
 	return l_test_run();
 }
