432cb4513ddc8aa5144ec2adea408dd237f657e6
Marcel Holtmann <marcel@holtmann.org>
2025-03-19 08:55:20 +0100

    unit: Add more test cases for SHA-256, SHA-384 and SHA-512

--- unit/test-checksum.c
+++ unit/test-checksum.c
@@ -236,6 +236,114 @@ static const struct sha_test sha224_test4 = {
 		  "e0e0e2ab14e6b8df265fc0b3",
 };
 
+static const struct sha_test sha256_test1 = {
+	.type	= L_CHECKSUM_SHA256,
+	.msg	= "abc",
+	.hash	= "ba7816bf8f01cfea414140de5dae2223"
+		  "b00361a396177a9cb410ff61f20015ad",
+};
+
+static const struct sha_test sha256_test2 = {
+	.type	= L_CHECKSUM_SHA256,
+	.msg	= "",
+	.hash	= "e3b0c44298fc1c149afbf4c8996fb924"
+		  "27ae41e4649b934ca495991b7852b855",
+};
+
+static const struct sha_test sha256_test3 = {
+	.type	= L_CHECKSUM_SHA256,
+	.msg	= "abcdbcdecdefdefgefghfghighijhijk"
+		  "ijkljklmklmnlmnomnopnopq",
+	.hash	= "248d6a61d20638b8e5c026930c3e6039"
+		  "a33ce45964ff2167f6ecedd419db06c1",
+};
+
+static const struct sha_test sha256_test4 = {
+	.type	= L_CHECKSUM_SHA256,
+	.msg	= "abcdefghbcdefghicdefghijdefghijk"
+		  "efghijklfghijklmghijklmnhijklmno"
+		  "ijklmnopjklmnopqklmnopqrlmnopqrs"
+		  "mnopqrstnopqrstu",
+	.hash	= "cf5b16a778af8380036ce59e7b049237"
+		  "0b249b11e8f07a51afac45037afee9d1",
+};
+
+static const struct sha_test sha384_test1 = {
+	.type	= L_CHECKSUM_SHA384,
+	.msg	= "abc",
+	.hash	= "cb00753f45a35e8bb5a03d699ac65007"
+		  "272c32ab0eded1631a8b605a43ff5bed"
+		  "8086072ba1e7cc2358baeca134c825a7",
+};
+
+static const struct sha_test sha384_test2 = {
+	.type	= L_CHECKSUM_SHA384,
+	.msg	= "",
+	.hash	= "38b060a751ac96384cd9327eb1b1e36a"
+		  "21fdb71114be07434c0cc7bf63f6e1da"
+		  "274edebfe76f65fbd51ad2f14898b95b",
+};
+
+static const struct sha_test sha384_test3 = {
+	.type	= L_CHECKSUM_SHA384,
+	.msg	= "abcdbcdecdefdefgefghfghighijhijk"
+		  "ijkljklmklmnlmnomnopnopq",
+	.hash	= "3391fdddfc8dc7393707a65b1b470939"
+		  "7cf8b1d162af05abfe8f450de5f36bc6"
+		  "b0455a8520bc4e6f5fe95b1fe3c8452b",
+};
+
+static const struct sha_test sha384_test4 = {
+	.type	= L_CHECKSUM_SHA384,
+	.msg	= "abcdefghbcdefghicdefghijdefghijk"
+		  "efghijklfghijklmghijklmnhijklmno"
+		  "ijklmnopjklmnopqklmnopqrlmnopqrs"
+		  "mnopqrstnopqrstu",
+	.hash	= "09330c33f71147e83d192fc782cd1b47"
+		  "53111b173b3b05d22fa08086e3b0f712"
+		  "fcc7c71a557e2db966c3e9fa91746039",
+};
+
+static const struct sha_test sha512_test1 = {
+	.type	= L_CHECKSUM_SHA512,
+	.msg	= "abc",
+	.hash	= "ddaf35a193617abacc417349ae204131"
+		  "12e6fa4e89a97ea20a9eeee64b55d39a"
+		  "2192992a274fc1a836ba3c23a3feebbd"
+		  "454d4423643ce80e2a9ac94fa54ca49f",
+};
+
+static const struct sha_test sha512_test2 = {
+	.type	= L_CHECKSUM_SHA512,
+	.msg	= "",
+	.hash	= "cf83e1357eefb8bdf1542850d66d8007"
+		  "d620e4050b5715dc83f4a921d36ce9ce"
+		  "47d0d13c5d85f2b0ff8318d2877eec2f"
+		  "63b931bd47417a81a538327af927da3e",
+};
+
+static const struct sha_test sha512_test3 = {
+	.type	= L_CHECKSUM_SHA512,
+	.msg	= "abcdbcdecdefdefgefghfghighijhijk"
+		  "ijkljklmklmnlmnomnopnopq",
+	.hash	= "204a8fc6dda82f0a0ced7beb8e08a416"
+		  "57c16ef468b228a8279be331a703c335"
+		  "96fd15c13b1b07f9aa1d3bea57789ca0"
+		  "31ad85c7a71dd70354ec631238ca3445",
+};
+
+static const struct sha_test sha512_test4 = {
+	.type	= L_CHECKSUM_SHA512,
+	.msg	= "abcdefghbcdefghicdefghijdefghijk"
+		  "efghijklfghijklmghijklmnhijklmno"
+		  "ijklmnopjklmnopqklmnopqrlmnopqrs"
+		  "mnopqrstnopqrstu",
+	.hash	= "8e959b75dae313da8cf4f72814fc143f"
+		  "8f7779c6eb9f7fa17299aeadb6889018"
+		  "501d289e4900f7e4331b99dec4b5433a"
+		  "c7d329eeb6dd26545e96e55b874be909",
+};
+
 static void test_sha(const void *data)
 {
 	const struct sha_test *test = data;
@@ -361,6 +469,18 @@ int main(int argc, char *argv[])
 	add_sha_test("SHA-224/2", &sha224_test2);
 	add_sha_test("SHA-224/3", &sha224_test3);
 	add_sha_test("SHA-224/4", &sha224_test4);
+	add_sha_test("SHA-256/1", &sha256_test1);
+	add_sha_test("SHA-256/2", &sha256_test2);
+	add_sha_test("SHA-256/3", &sha256_test3);
+	add_sha_test("SHA-256/4", &sha256_test4);
+	add_sha_test("SHA-384/1", &sha384_test1);
+	add_sha_test("SHA-384/2", &sha384_test2);
+	add_sha_test("SHA-384/3", &sha384_test3);
+	add_sha_test("SHA-384/4", &sha384_test4);
+	add_sha_test("SHA-512/1", &sha512_test1);
+	add_sha_test("SHA-512/2", &sha512_test2);
+	add_sha_test("SHA-512/3", &sha512_test3);
+	add_sha_test("SHA-512/4", &sha512_test4);
 
 	l_test_add_data_func("aes-cmac-1", &aes_cmac_test1, test_aes_cmac,
 						L_TEST_FLAG_ALLOW_FAILURE);
